798345880fa3f138b1f45d6815d1ce07fae63d0b
chahteacher <chahteacher@AC02VH017GL27-5.local>
2021-06-30 18:41:43 -0400

    Collaborators: Benson Chaouiki Dwayne Lyuba Michele

--- 1/Nim.java
+++ 1/Nim.java
@@ -1,6 +1,3 @@
-/*
-program incomplete - I need to rereadx5 and see more examples
-*/
 
 /*
 Collaborators:
@@ -32,72 +29,63 @@ a move consists of removing 1-3 stones from the bag
 
 import java.io.*;
 import java.util.*;
-import java.util.Scanner; //not necessary because of *
-
+import java.util.Scanner; //not necessary
 public class Nim{
   public static void main(String[] args){
-
-//create and initialize variable to store the number of stones
+    //create variable to store the number of stones
     int stonesRemoved = 0;
-//create and initialize variable for current number of stones
+    //create variable for current number of stones
     int stonesRemain = 12;
-
-/*
-not sure what this means: if current number of stones is gt 0
-variable valid is false?? while not valid = true, prompt user?
-*/
     while (stonesRemain > 0){
       boolean valid = false;
       while (!valid){
-
-    //ask user for number of stones 1-3 out of 12 stones total
-    System.out.println("There are 12 stones, please choose between 1-3");
-
-    //create a scanner class
-    Scanner in = new Scanner(System.in);
-
-    //read in stones (nextIn-stones rem)
-    stonesRemoved = in.nextInt();
-
-    /*
-    check input until it is valid - use loop??; can someone explain this
-    */
-    valid = checkValid(stonesRemain, stonesRemoved);
-    if (!valid)
-    {
-      System.out.println("Invalid entry.");
+      //ask user for number of stones 1-3 out of 12 stones total
+      System.out.println("There are " + stonesRemain + " stones, please choose between 1-3");
+      //create a scanner class
+      Scanner in = new Scanner(System.in);
+      //read in stones (nextIn-stones rem)
+      stonesRemoved = in.nextInt();
+      //check if input until it is valid - use loop
+      valid = checkValid(stonesRemain, stonesRemoved);
+      if(!valid)
+      {
+        System.out.println("Invalid entry.");
+      }
     }
-    }
-
-    /*
-    -if valid, then calculate number of stones left
-    -can somone explain this:
-    -stonesRemain -= stonesRemoved means stonesRemain
-     equals stonesRemain minus stonesRemoved
-    */
-    stonesRemain -= stonesRemoved
-
+    //if valid then calculate number of stones left
+    stonesRemain -= stonesRemoved;
     //check if user wins
-    if(stonesRemain <= 0) //shouldn't/couldn't this be if(stonesRemain = 0)
+    if(stonesRemain <= 0 )
     {
       System.out.println("User wins!");
     }
-    else
-    //will finish tomrrow--
-    //create variable for current number of stones
-    //if valid then calculate number of stones left
-    //AI generates random number between 1-3
-    //tell user how many stones AI removed and how many remain - print
+    else     //AI generates random number between 1-3
+    {
+     // random is in the range 1-3 (including 3). T
+     //The parentheses are necessary!
+     //Source: https://csawesome.runestone.academy/runestone/books/published/
+     //csawesome/Unit2-Using-Objects/topic-2-9-Math.html
+      int random = (int)(Math.random()*3) + 1;
+
+      //tell user how many stones AI removed and how many remain - print
+      System.out.println("AI plays: " + random);
+      stonesRemain -= random;
+      System.out.println("Remaining: " + stonesRemain );
+      if(stonesRemain <= 0)
+      {
+        System.out.println("AI wins!");
+      }
+    }
+
     //check winner after each turn; after user and after AI
     //make loop
     }
   }
-  public static boolean checkValid(int stoneRemain, int stonesRemoved){
+  public static boolean checkValid(int stonesRemain, int stonesRemoved){
     if (stonesRemoved <= stonesRemain && (stonesRemoved == 3 || stonesRemoved == 2
     || stonesRemoved == 1)){
       return true;
     }
     else return false;
     }
-
   }
