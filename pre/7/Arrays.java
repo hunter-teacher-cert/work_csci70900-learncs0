f10a4e1968a64b824c14ea6e3746f12067b4eba7
chahteacher <chahteacher@AC02VH017GL27-5.local>
2021-07-01 15:40:29 -0400

    variable size error - learning arrays

---
+++ pre/7/Arrays.java
@@ -0,0 +1,33 @@
+//Write a program named Arrays.java and in it solve exercises 1, 4 and 5 from
+//chapter 8 in Think Java
+
+//Exercise 1   Practice encapsulation Starting with the code
+//in Section 8.6, write a method called powArray that takes
+//a double array, a, and returns a new array that contains
+//the elements of a squared. Generalize it to take a
+//second argument and raise the elements of a to the given power.
+
+
+//indep practice section 8.2
+import java.io.*;
+import java.util.*;
+
+public class Arrays{
+public static double powArray(double [] a) {
+//count refer to an array of four integers
+//values refer to an array of double, where
+//the number of elements in values depends on the value of size
+  //counts = new int[4];
+  //values = new double[size];
+
+//can also declare the variable and create the array
+//in a single line of code:
+int[] counts = new int[4];
+double[] values = new double[size];
+System.out.println("The zeroth element is  " +counts[0]);
+counts[0] = 7;
+counts[1] = counts[0] * 2;
+counts[2]++;
+counts[3] -= 60;
+  }
+}
