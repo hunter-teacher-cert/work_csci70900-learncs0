403add35b4b3c66b3c7dc299f6ea48d44f45f1f0
chahteacher <chahteacher@AC02VH017GL27-5.local>
2021-07-05 08:10:01 -0400

    learning to use loops with arrays-messy file-will clean up as I learn it

--- pre/7/Arrays.java
+++ pre/7/Arrays.java
@@ -1,5 +1,9 @@
+
 //Write a program named Arrays.java and in it solve exercises 1, 4 and 5 from
 //chapter 8 in Think Java
+
+//Consult:
+//https://www.geeksforgeeks.org/arrays-in-java/
 
 //Exercise 1   Practice encapsulation Starting with the code
 //in Section 8.6, write a method called powArray that takes
@@ -8,8 +12,92 @@
 //second argument and raise the elements of a to the given power.
 
 
-//indep practice section 8.2
+//8.6 Array traversal
 /*
+class Arrays
+{
+  public static void main (String[] args){
+    //double[] a = new double[]; //declare & create double type array a; cells init to 0
+
+    //declare, construct, and initialize the array all in one statement:
+    double[] a = {1, 2, 3, 4};
+    double[] asq = new double[]; //declare & create double type array asq; cells init to 0
+
+
+
+
+  }
+
+
+}
+for (int i = 0; i < a.length; i++) {
+    a[i] = Math.pow(a[i], 2.0);
+}
+
+//Looping through the elements of an array is called a traversal.
+//the following method takes an int array and an integer value,
+//and it returns the index where the value appears:
+
+public static int search(double[] a, double target) {
+    for (int i = 0; i < a.length; i++) {
+        if (a[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+class Arrays
+{
+  public static void main ( String[] args )
+  {
+    int[] valA = { 12, 23, 45, 56 };
+
+    int[] valB = new int[4];
+
+    valB[ 0 ]  = valA[ 0 ] ;
+    valB[ 1 ]  = valA[ 1 ] ;
+    valB[ 2 ]  = valA[ 2 ] ;
+    valB[ 3 ]  = valA[ 3 ] ;
+
+    System.out.println("valA [0] has " + valA[0]);
+    System.out.println("valA [1] has " + valA[1]);
+    System.out.println("valA [2] has " + valA[2]);
+    System.out.println("valA [3] has " + valA[3]);
+
+    System.out.println("valB [0] has " + valB[0]);
+    System.out.println("valB [1] has " + valB[1]);
+    System.out.println("valB [2] has " + valB[2]);
+    System.out.println("valB [3] has " + valB[3]);
+
+   }
+}
+
+
+
+
+
+
+
+public static void printArray(int[] a) {
+    System.out.print("{" + a[0]);
+    for (int i = 1; i < a.length; i++) {
+        System.out.print(", " + a[i]);
+    }
+    System.out.println("}");
+}
+
+
+
+
+
+
+
+
+
+/*
+
 import java.io.*;
 import java.util.*;
 
@@ -33,15 +121,88 @@ counts[3] -= 60;
   }
 }
 //error with size
-*/
+
+
+
+
 
 
 //Kjell practice
+
+
+//copying values between Arrays
+/*
+class Arrays
+{
+  public static void main ( String[] args )
+  {
+    int[] valA = { 12, 23, 45, 56 };
+
+    int[] valB = new int[4];
+
+    valB[ 0 ]  = valA[ 0 ] ;
+    valB[ 1 ]  = valA[ 1 ] ;
+    valB[ 2 ]  = valA[ 2 ] ;
+    valB[ 3 ]  = valA[ 3 ] ;
+
+    System.out.println("valA [0] has " + valA[0]);
+    System.out.println("valA [1] has " + valA[1]);
+    System.out.println("valA [2] has " + valA[2]);
+    System.out.println("valA [3] has " + valA[3]);
+
+    System.out.println("valB [0] has " + valB[0]);
+    System.out.println("valB [1] has " + valB[1]);
+    System.out.println("valB [2] has " + valB[2]);
+    System.out.println("valB [3] has " + valB[3]);
+
+   }
+}
+*/
+/*
+Output
+valA [0] has 12
+valA [1] has 23
+valA [2] has 45
+valA [3] has 56
+valB [0] has 12
+valB [1] has 23
+valB [2] has 45
+valB [3] has 56
+*/
 
 //Using an expression for an array index
 //organizing the data into arrays
 //processing that data using variables as indexes
 
+//more complicated example
+/*
+class Arrays
+{
+  public static void main ( String[] args )
+  {
+    double[] val = new double[4];
+
+    val[0] =  1.5;
+    val[1] = 10.0;
+    val[2] = 15.5;
+
+    int j  = 3;
+
+    // same as val[3] = val[2] + val[1]
+    //val [3] = val [3-1=2] + val [3-2]
+    //val [3] = 15.5+10 == 25.5
+    val[j] = val[j-1] + val[j-2];
+
+
+    System.out.println( "val[" + j + "] == " + val[j] );
+
+   }
+}*/
+//output val[3] == 25.5
+
+
+//simple example
+/*
 class Arrays
 {
   public static void main ( String[] args )
@@ -60,6 +221,12 @@ class Arrays
     System.out.println( "cell 1: " + val[ j   ] );//j=3-2=1 index 1 = 1.43
    }
 }
+/*
+Output
+cell 3: 0.0
+cell 2: 2.98
+cell 1: 1.43
+*/
 
 //In the following, the array object is constructed and each cell is initialized
 //to 0. Then some assignment statements explicitly change some cells:
@@ -89,3 +256,44 @@ stuff[2] has 14
 stuff[3] has 0
 stuff[4] has 0
 */
+
+
+//-----------------------
+//indep practice section 8.2
+
+import java.io.*;
+import java.util.*;
+
+public class Arrays{
+  public static void main ( String[] args ){
+    int[] counts = new int[4];
+    counts[0] = 7;
+    counts[1] = counts[0] * 2;
+    counts[2]++;
+    counts[3] -= 60;
+    //System.out.println("The zeroth element is " + counts[0]);
+    //Output of line above The zeroth element is 7
+
+    //You can use any expression as an index,
+    //as long as it has type int.
+    //One of the most common ways to index an array is with a loop variable. For example:
+    //This while loop counts from 0 up to 4.
+    //When i is 4, the condition fails and the loop terminates.
+    //So the body of the loop is only executed when i is 0, 1, 2, and 3.
+    //Each time through the loop we use i as an index
+    //into the array, displaying the ith element.
+      int i = 0;
+      while (i < 4) {
+    System.out.println(counts[i]);
+    i++;
+
+    //output of above 7
+    //14
+    //1
+    //-60
+
+}
+
+  }
+
+}
