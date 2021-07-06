f6f6f4cf6d83774a9f533744febd43f3bc12ef76
chahteacher <chahteacher@AC02VH017GL27-5.local>
2021-07-06 12:42:46 -0400

    practice objects

---
+++ 5/Driver.java
@@ -0,0 +1,20 @@
+// heading
+//Victoria - vmiller77
+//Brian - brianmueller
+//Michele -learncs0
+
+import java.io.*;
+import java.util.*;
+
+public class Driver {
+
+  public static void main(String[] args) {
+
+    //declare a var of appropriate type to assign an instance of Time to
+    Time t;
+
+    //assign var the address of a newly-apportioned Time object
+    t = new Time();
+  }//end main()
+
+}//end class
