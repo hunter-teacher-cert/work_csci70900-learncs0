ffdcc9d736778eda6f388c7ab97d297b5ed7e6bc
chahteacher <chahteacher@AC02VH017GL27-5.local>
2021-07-06 12:39:59 -0400

    working with objects

---
+++ 5/Time.java
@@ -0,0 +1,13 @@
+// heading
+//Victoria - vmiller77
+//Brian - brianmueller
+//Michele -learncs0
+
+import java.io.*;
+import java.util.*;
+
+public class Time {
+
+  // your implementation of class Time here
+
+}//end class
