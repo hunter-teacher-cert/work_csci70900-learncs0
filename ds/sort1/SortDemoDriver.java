b1b2b839376eb6c3cb15b4b4663c3349b5448f72
chahteacher <chahteacher@mpchah.local>
2021-07-09 11:43:06 -0400

    sort

---
+++ ds/sort1/SortDemoDriver.java
@@ -0,0 +1,34 @@
+//Daiana
+//Emma
+//Chaouki
+//Michele
+import java.io.*;
+import java.util.*;
+
+public class SortDemoDriver {
+    public static void main(String[] args) {
+
+	SortDemo sd = new SortDemo();
+	System.out.println(sd);
+
+	SortDemo sd2 = new SortDemo(20);
+	System.out.println(sd2);
+
+
+	int i;
+	i = sd.findSmallestIndex(0);
+	System.out.println("sd[" + i + "] = "+sd.get(i)+" : " + sd);
+	i = sd2.findSmallestIndex(0);
+	System.out.println("sd2[" + i + "] = "+sd2.get(i)+" : " + sd2);
+
+  /* part 2 test lines */
+
+
+	/* part 3 test lines */
+
+	  System.out.println(sd);
+	  sd.sort();
+	  System.out.println(sd);
+
+    }
+}
