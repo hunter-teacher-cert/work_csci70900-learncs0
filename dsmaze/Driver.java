ea6cca3a6561f44be826a0465a0ee34e036cee0b
chahteacher <chahteacher@mpchah.local>
2021-07-14 12:23:27 -0400

    Eric thank you for teaching us

--- dsmaze/Driver.java
+++ dsmaze/Driver.java
@@ -1,19 +1,32 @@
+/**
+ * Team
+  * Eric Liu
+  * Michell Best
+ * Michelle Persaud
+ */
+
 import java.io.*;
 import java.util.*;
 
 public class Driver{
 
     public static void main(String[] args) {
-	boolean solved;
+		boolean solved;
 
-	Maze m = new Maze("maze.dat");
-	System.out.println(m);
-	solved = m.solve(1,1);
+		// Maze m = new Maze("maze.dat");//create new instance/object of Maze
+		// //Maze n = new Maze("maze.dat");
+		// System.out.println(m);
+		// solved = m.solve(3,3);
 
-	/*
-	  Knights k - new Knights(5);
-	  solved = k.solve(1,1);
-	*/
+		//Start randomly on the user accessible board
+		//test random start
+		int boardSize = 5;
+		Random rand = new Random();
 
-    }
+		Knights k = new Knights(boardSize);
+		solved = k.solve(rand.nextInt(boardSize)+2,rand.nextInt(boardSize)+2,1);
+
+
+
+    }//end main()
 }
