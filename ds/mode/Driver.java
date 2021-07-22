99b4fa87c73f71be8db6c5b263615839c3c7834c
chahteacher <chahteacher@mpchah.local>
2021-07-22 11:47:00 -0400

    thank you Alex and Tiffany

---
+++ ds/mode/Driver.java
@@ -0,0 +1,26 @@
+/*
+Tiffany W.
+Alex M.
+Michele P
+*/
+
+
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+
+  public static void main(String[] args) {
+    Mode m = new Mode();
+
+    System.out.println(m);
+    //System.out.println("smallest value: " + m.findSmallestValue());
+
+    int modeFound = m.calcMode();
+    System.out.println("mode: " + modeFound);
+    System.out.println("number of times mode appears: " + m.frequency(modeFound));
+
+
+  }
+
+}
