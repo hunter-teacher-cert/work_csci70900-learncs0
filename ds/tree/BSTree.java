60639e0c02e698269b99e7b11993805b9f771b77
chahteacher <chahteacher@mpchah.local>
2021-07-23 12:27:57 -0400

    added comments and code from video

--- ds/tree/BSTree.java
+++ ds/tree/BSTree.java
@@ -1,35 +1,39 @@
 import java.io.*;
 import java.util.*;
 
-public class BSTree {
+public class BSTree { //start with constructor
     private TreeNode root;
 
     public BSTree(){
-	root = null;
+	root = null; //make the root null
     }
 
     public int search(int key){
 	TreeNode current = root;
 
-	while (/* what goes here */){
+	while (current != null){
 	    int currentValue = current.getValue();
 	    if (currentValue == key){
 		// do something
+        return key;
 	    } else if (currentValue < key){
+        current = current.getRight();
 		// do something else
 	    } else {
 		// do yet another thing
+    current = current.getLeft();
 		}
 	}
 	// do we really want to return -1?
-	return -1;
+	//return -1;
+  throw new NullPointerException();
     }
 
-    public void seed(){
-	TreeNode t;
+    public void seed(){ //manually building the tree
+	TreeNode t; //start with tree node t
 
-	t = new TreeNode(10);
-	root = t;
+	t = new TreeNode(10); //make a new node with a 10 in it; it's left and right are null
+	root = t; //root points to t
 	t = new TreeNode(5);
 	root.setLeft(t);
 	t = new TreeNode(20);
--- ds/tree/TreeNode.java
+++ ds/tree/TreeNode.java
@@ -3,21 +3,21 @@ import java.io.*;
 import java.util.*;
 
 public class TreeNode{
-    private int data;
-    private TreeNode left,right;
+    private int data; //stuff
+    private TreeNode left,right; //pointers
 
-    public TreeNode(){
-	this(0);
+    public TreeNode(){ //first constructor
+	this(0); //calls ?
 	left = null;
 	right = null;
     }
 
-    public TreeNode(int data){
+    public TreeNode(int data){ //second constructor
 	this.data = data;
 	left = null;
 	right = null;
     }
-
+    //create getters and setters
     public void setData(int data){
 	this.data = data;
     }
