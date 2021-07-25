77a20466898527ad3f58acef2a4cd25d91c0ff04
chahteacher <chahteacher@mpchah.local>
2021-07-25 17:00:42 -0400

    used video to insert comments and code for part 2

--- ds/tree/BSTree.java
+++ ds/tree/BSTree.java
@@ -9,13 +9,15 @@ public class BSTree { //start with constructor
     }
 
     public int search(int key){
-	TreeNode current = root;
+          //start current node at the root
+	TreeNode current = root; //start with a temporary node called current
 
-	while (current != null){
-	    int currentValue = current.getValue();
+	while (current != null){ //while not null keep searching until value is found
+	    int currentValue = current.getData(); //should not be getValue
 	    if (currentValue == key){
+        return currentValue;
 		// do something
-        return key;
+        //return key; //return the value in the tree;
 	    } else if (currentValue < key){
         current = current.getRight();
 		// do something else
@@ -26,6 +28,7 @@ public class BSTree { //start with constructor
 	}
 	// do we really want to return -1?
 	//return -1;
+  //throw an exception if it is not found
   throw new NullPointerException();
     }
 
@@ -34,20 +37,28 @@ public class BSTree { //start with constructor
 
 	t = new TreeNode(10); //make a new node with a 10 in it; it's left and right are null
 	root = t; //root points to t
-	t = new TreeNode(5);
-	root.setLeft(t);
-	t = new TreeNode(20);
-	root.setRight(t);
+	t = new TreeNode(5); //t points to a new node which has a data of 5
+	root.setLeft(t); //connect-link 10 to 5
+	t = new TreeNode(20); //t assigned a new node; t has a new node with a 20 in it
+	root.setRight(t); //point the root's right to it
 
-	root.getLeft().setRight( new TreeNode(8));
+	root.getLeft().setRight( new TreeNode(8)); //root get left set right has 8
 
-	t = new TreeNode(15);
+	t = new TreeNode(15); //make  a new node with 15 in it
 	root.getRight().setLeft(t);
 
 	t = new TreeNode(22);
-	root.getRight().setRight(t);
+	root.getRight().setRight(t);//root, get right, set right; make node with 22
 
 	}
 
 
 }
+//
+//in this tree given any node
+//every node in left subtree has a lower/lesservalue less;
+//every node in right subtree has a greater value
+//tree has an ordering
+//        10
+//    5         20
+//      8     15    22
--- ds/tree/Driver.java
+++ ds/tree/Driver.java
@@ -7,18 +7,23 @@ public class Driver {
 		BSTree t = new BSTree();
 
 		t.seed();
-		/*
+
 		int value;
-		value = t.search(10);
+		value = t.search(10); //look for 10
 		System.out.println(value);
-		value = t.search(15);
+		value = t.search(15); //look for 15
 		System.out.println(value);
 		try {
-			value = t.search(17);
+			value = t.search(17); //look for 17 - should throw exception
 			System.out.println(value);
 		} catch (NullPointerException e) {
 			System.out.println("17 not in tree");
 		}
-		*/
+
 	}
 }
+/*
+10
+15
+17 not in tree
+*/
