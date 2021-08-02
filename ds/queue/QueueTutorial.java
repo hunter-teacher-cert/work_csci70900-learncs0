732115a8fa24e2c300a19c58e3d6a0b19b908b85
chahteacher <chahteacher@mpchah.local>
2021-08-02 10:36:32 -0400

    queue tutorial

--- ds/queue/QueueTutorial.java
+++ ds/queue/QueueTutorial.java
@@ -1,5 +1,9 @@
 /*
-I am practicing
+I am practicing using these tutorials
+tutorials:
+https://www.geeksforgeeks.org/queue-poll-method-in-java/
+ https://www.youtube.com/watch?v=91CMnJeHJVc
+
 Queue Folder: ds/queue
 
 Create a class that represents a queue. This class should use a linked list that you build (not the built in Java class) with at least two pointers. Your class should implement the following methods:
@@ -11,9 +15,7 @@ x = front() <-- return but don't remove the value at the front
 isEmpty() <-- returns true if the queue is empty
 size() <-- returns the number of items in the queue
 
-tutorials:
-https://www.geeksforgeeks.org/queue-poll-method-in-java/
- https://www.youtube.com/watch?v=91CMnJeHJVc
+
 */
 
 import java.io.*;
@@ -38,9 +40,24 @@ public static void main(String[] args){
   /*[Juan, Lizbeth, Charlie, Hadeel]
   Queue front: Juan
 */
+  System.out.println(studentLine);
+  /*output
+  [Lizbeth, Charlie, Hadeel]
+  */
+  System.out.println(studentLine.peek()); /*output:  previews next removal Lizbeth*/
 
+  System.out.println("Size:  " + studentLine.size()); //Size:  3
 
+  System.out.println("contains Lizbeth: " + studentLine.contains("Lizbeth"));
 
+  System.out.println("contains Marco: " + studentLine.contains("Marco"));
+  /*output
+  contains Lizbeth: true
+  contains Marco: false
+  */
+  //change to an array and get an element
+  System.out.println("index 1 is: " + studentLine.toArray()[1]);
+  /*output index 1 is: Charlie*/
 
 
 }//void
